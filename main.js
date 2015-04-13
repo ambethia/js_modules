19991ce9301052c283e228780fe048c9bde92497
Jason L Perry <jasper@ambethia.com>
2015-04-13 13:15:38 -0400

    Common.js using node

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
-function main () {
+var color = require("./color.js");
 
-  var a = document.getElementById('a');
-  var b = document.getElementById('b');
-  var c = document.getElementById('c');
-  var r = document.querySelector('input[type=range]');
+var a = color.random();
+var b = color.random();
+var c = color.mix(a, b);
 
-  function setSwatchColor(swatch, aColor) {
-    swatch.style.backgroundColor = aColor;
-    swatch.textContent = aColor;
-  };
-
-  function randomizeSwatch(swatch) {
-    setSwatchColor(swatch, random());
-  }
-
-  function mixSwatches() {
-    var f = parseFloat(r.value);
-    setSwatchColor(b, mix(a.textContent, c.textContent, f));
-  };
-
-  a.addEventListener('click', function(e) { randomizeSwatch(e.target); mixSwatches(); });
-  c.addEventListener('click', function(e) { randomizeSwatch(e.target); mixSwatches(); });
-  r.addEventListener('input', mixSwatches);
-
-  randomizeSwatch(a);
-  randomizeSwatch(c);
-  mixSwatches();
-};
-
-document.addEventListener('DOMContentLoaded', main);
+console.log(a + " mixed with " + b + " is " + c + '.');
